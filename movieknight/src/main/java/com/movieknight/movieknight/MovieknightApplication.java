3bdd92ea899a34e379b2881d3665866859f0782d
Frans Herrstrom <frans.herrstrom@gmail.com>
2018-12-12 13:50:17 +0100

    first commit

---
+++ movieknight/src/main/java/com/movieknight/movieknight/MovieknightApplication.java
@@ -0,0 +1,12 @@
+package com.movieknight.movieknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieknightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieknightApplication.class, args);
+	}
+}

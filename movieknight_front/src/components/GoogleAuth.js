c210f78dc132afcb4fd1c5851ba1980f52d27e18
Frans Herrstrom <frans.herrstrom@gmail.com>
2018-12-19 13:41:56 +0100

    signInCallBack using react, forced

--- movieknight_front/src/components/GoogleAuth.js
+++ movieknight_front/src/components/GoogleAuth.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-
+import axios from 'axios';
 export default class GoogleAuth extends Component {
-
   state = {
     isSignedIn: null
   };
@@ -26,29 +25,75 @@ export default class GoogleAuth extends Component {
     });
   }
 
-  onAuthChange=()=>{
-      this.setState({
-          isSignedIn:window.gapi.auth2.getAuthInstance().isSignedIn.get()
-      })
-  }
+  signInCallback = authResult => {
+    if (authResult['code']) {
+      // Send the code to the server
+      window.$.ajax({
+        type: 'POST',
+        url: 'http://localhost:6969/google',
+        // Always include an `X-Requested-With` header in every AJAX request,
+        // to protect against CSRF attacks.
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        contentType: 'application/octet-stream; charset=utf-8',
+        success: function(result) {
+          // Handle or verify the server response.
+          console.log(result);
+        },
+        processData: false,
+        data: authResult['code']
+      });
+    } else {
+      // There was an error.
+    }
+  };
 
-onSignIn=()=>{
-    window.gapi.auth2.getAuthInstance().signIn();
-}
+  search = query => {
+    let url = 'http://localhost:6969/omdb/movies/search/?s=' + query;
+    axios.get(url).then(res => {
+      const movieList = res.data;
+      if (movieList.Search != null) {
+        this.setState({ movieList: movieList.Search });
+      } else {
+        this.setState({ movieList: [] });
+      }
+    });
+  };
 
-onSignOut=()=>{
+  onAuthChange = () => {
+    this.setState({
+      isSignedIn: window.gapi.auth2.getAuthInstance().isSignedIn.get()
+    });
+  };
+
+  onSignIn = () => {
+    window.gapi.auth2
+      .getAuthInstance()
+      .grantOfflineAccess()
+      .then(this.signInCallback);
+  };
+
+  onSignOut = () => {
     window.gapi.auth2.getAuthInstance().signOut();
-}
-
+  };
 
   renderAuthButoon = () => {
     if (this.state.isSignedIn === null) {
       return null;
     }
     if (this.state.isSignedIn) {
-      return <Button onClick={this.onSignOut} variant="contained" color="secondary"><i className="fab fa-google"></i>{" "}Sign out</Button>;
+      return (
+        <Button onClick={this.onSignOut} variant="contained" color="secondary">
+          <i className="fab fa-google" /> Sign out
+        </Button>
+      );
     } else {
-      return <Button onClick={this.onSignIn} variant="contained" color="secondary" ><i className="fab fa-google"></i> {" "}Sign in</Button>;
+      return (
+        <Button onClick={this.onSignIn} variant="contained" color="secondary">
+          <i className="fab fa-google" /> Sign in
+        </Button>
+      );
     }
   };
 
